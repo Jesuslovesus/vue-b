c93831bb9ea16b05999564e3f6a862657218adcf
Jesuslovesus <1@qq.com>
2017-12-14 16:08:29 +0800

    第一次提交

---
+++ src/view/layout/navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="navbar">
+    <div>
+      <el-button round>圆形按钮</el-button>
+      <el-button type="primary" round>主要按钮</el-button>
+      <el-button type="success" round>成功按钮</el-button>
+      <el-button type="info" round>信息按钮</el-button>
+      <el-button type="warning" round>警告按钮</el-button>
+      <el-button type="danger" round>危险按钮</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        config: {
+          value: 'https://www.baidu.com',
+          imagePath: './examples/assets/logo.png',
+          filter: 'color'
+        },
+        downloadButton: false
+      }
+    },
+    components: {
+    },
+    mounted() {
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped >
+
+</style>
