f937ae7137b4b1cbe06ef98e14220ea7b29cd60d
Jesuslovesus <1@qq.com>
2017-12-14 18:12:13 +0800

    1
    1

--- vue-b/.eslintrc.js
+++ vue-b/.eslintrc.js
@@ -8,6 +8,8 @@ module.exports = {
   },
   env: {
     browser: true,
+    node: true,
+    es6: true
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
@@ -17,6 +19,13 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
+    // always-multiline：多行模式必须带逗号，单行模式不能带逗号  
+    "comma-dangle": [2, "never"], 
+    "no-empty": 2, 
+    "no-multi-spaces": 2, 
+    "no-mixed-spaces-and-tabs": [2,"smart-tabs"],
+    
+
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
---
+++ vue-b/src/views/layout/layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="app-wrapper">
+    <div>
+    
+    </div>
+    <!-- 内容 -->
+    <div class="main-container">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import { AppMain } from './index'
+export default {
+  name: 'layout',
+  components: {
+    AppMain
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
