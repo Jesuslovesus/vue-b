462c63159507716fe9b6c72c8a57c6ab254b0745
Jesuslovesus <1@qq.com>
2017-12-15 09:47:47 +0800

    eslint

--- vue-b/.eslintrc.js
+++ vue-b/.eslintrc.js
@@ -24,11 +24,28 @@ module.exports = {
     "no-empty": 2, 
     "no-multi-spaces": 2, 
     "no-mixed-spaces-and-tabs": [2,"smart-tabs"],
-    
-
+    "no-trailing-spaces": 2,
+    "padded-blocks": [2,"never"],
+    "quotes": ["error", "single"],
+    "no-console": 2, 
+    "no-extra-semi": 2, 
+    "indent": ["error", 2, { "SwitchCase": 1 }], 
+    // 双峰驼命名格式  
+    "camelcase": [2,{"properties": "never"}],
+    "key-spacing": [2,{"beforeColon": false, "afterColon": true}],
+    "space-before-function-paren":[
+      2,
+      {"anonymous": "naver","named": "never"}
+    ],
+    "newline-after-var": 0,
+    "no-inline-comments": 2,
+    "no-multiple-empty-lines": [2, { "max": 2 }],
+    "semi": [2, "never"], 
+    "no-var": 0,
+    "no-redeclare": 1,  
     // allow async-await
-    'generator-star-spacing': 'off',
+    // 'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
